3b6d90049d611ed3c30f8ec880c8c06482181584
Anthony Cruz <anthonyleierlw@gmail.com>
2024-01-16 10:49:54 -0300

    Pega dois números da entrada do usuário e compara para encontrar o maior

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
-fn main() {
-    let number1 = 2;
-    let number2 = 2;
+use std::io;
 
-    if number1 > number2 {
-        println!("Número 1 é maior")
-    } else if number2 > number1 {
-        println!("Número 2 é maior")
+fn convert_to_int(data_input: &String) -> i32 {
+    let x = data_input.trim().parse::<i32>().unwrap();
+    return x;
+}
+
+fn main() {
+    println!("Insira o primeiro número:");
+    let mut number1 = String::new();
+    io::stdin()
+        .read_line(&mut number1)
+        .expect("Não foi possível ler o número 1");
+
+    println!("Insira o segundo número:");
+    let mut number2 = String::new();
+    io::stdin()
+        .read_line(&mut number2)
+        .expect("Não foi possível ler o número 2");
+
+    let result1 = convert_to_int(&number1);
+    let result2 = convert_to_int(&number2);
+
+    if result1 > result2 {
+        println!("{} é maior que {}", result1, result2);
+    } else if result2 > result1 {
+        println!("{} é maior que {}", result2, result1);
     } else {
-        println!("Os números são iguais")
+        println!("{} e {} são iguais", result1, result2)
     }
 }
