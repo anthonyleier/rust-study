e1a1be43ce2072d9dbfe8e1127ed32cf25997bf4
Anthony Cruz <anthonyleierlw@gmail.com>
2024-01-16 11:03:53 -0300

    Soma de dígitos usando while

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,22 @@ fn convert_to_int(data_input: &String) -> i32 {
 }
 
 fn main() {
-    println!("Insira o primeiro número:");
-    let mut number1 = String::new();
+    let mut soma = 0;
+    let mut valor_entrada = String::new();
+
     io::stdin()
-        .read_line(&mut number1)
-        .expect("Não foi possível ler o número 1");
+        .read_line(&mut valor_entrada)
+        .expect("Não foi possível ler a entrada");
 
-    println!("Insira o segundo número:");
-    let mut number2 = String::new();
-    io::stdin()
-        .read_line(&mut number2)
-        .expect("Não foi possível ler o número 2");
+    let mut valor_i32 = convert_to_int(&valor_entrada);
 
-    let result1 = convert_to_int(&number1);
-    let result2 = convert_to_int(&number2);
+    while valor_i32 != 0 {
+        let resto = valor_i32 % 10;
 
-    if result1 > result2 {
-        println!("{} é maior que {}", result1, result2);
-    } else if result2 > result1 {
-        println!("{} é maior que {}", result2, result1);
-    } else {
-        println!("{} e {} são iguais", result1, result2)
+        soma += resto;
+
+        valor_i32 = valor_i32 / 10;
     }
+
+    println!("A soma é {}", soma);
 }
