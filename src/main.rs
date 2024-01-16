967231a54e329a57a3ef96a1cf8706d5b8e618fb
Anthony Cruz <anthonyleierlw@gmail.com>
2024-01-16 10:10:16 -0300

    Programa para calcular segundos trabalhados

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+const SECONDS_IN_MINUTE: u32 = 60;
 fn main() {
-    println!("Hello, world!");
+    const MINUTES_IN_HOUR: u32 = 60;
+    const SECONDS_IN_HOUR: u32 = SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
+
+    let total = 30;
+    let total_em_segundos = total * SECONDS_IN_HOUR;
+    println!("Trabalhou {} segundos!", total_em_segundos);
 }
